89d5797dbdd86b83639ff26bafa15a0a845597df
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-01-11 17:04:18 +0900

    テストページ追加

--- src/pages/channel.tsx
+++ src/pages/channel.tsx
@@ -1,4 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
 const Index: React.FC = () => {
-  return <div>test</div>
+  const [count, setCount] = React.useState(1)
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <Link href="/movie">動画</Link>
+    </div>
+  )
 }
 export default Index
---
+++ src/pages/movie.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+const Index: React.FC = () => {
+  return (
+    <div>
+      <Link href="/channel">チェンネル</Link>
+    </div>
+  )
+}
+export default Index
