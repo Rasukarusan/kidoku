293924b05f401c8263ce35ccedaa8a73438d603a
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-11 23:17:31 +0900

    index.tsxのPATH変更

--- next.config.js
+++ next.config.js
@@ -5,12 +5,4 @@ module.exports = {
     config.resolve.alias['@'] = path.join(__dirname, 'src')
     return config
   },
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/index',
-      },
-    ]
-  },
 }
