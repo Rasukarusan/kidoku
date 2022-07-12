933adf841d8218b989f3864b801cdaa2911c5a9f
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-12 09:29:38 +0900

    middlewareを分割

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
-
-export const config = {
-  matcher: '/sheet',
-}
+import { basicAuth } from '@/middlewares/basicAuth'
 
 export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization')
-  const url = req.nextUrl
-
-  if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
-
-    if (
-      user === process.env.BASIC_AUTH_USER &&
-      pwd === process.env.BASIC_AUTH_PASS
-    ) {
-      return NextResponse.next()
-    }
+  if (req.nextUrl.pathname.startsWith('/sheet')) {
+    return basicAuth(req)
   }
-  url.pathname = '/api/auth'
-
-  return NextResponse.rewrite(url)
+  return NextResponse.next()
 }
