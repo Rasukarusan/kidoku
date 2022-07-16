3695c6a09482fec352319af24c145f58c84eb9f5
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-16 10:50:56 +0900

    Basic認証削除

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { basicAuth } from '@/middlewares/basicAuth'
-
-export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith('/sheet')) {
-    return basicAuth(req)
-  }
-  return NextResponse.next()
-}
