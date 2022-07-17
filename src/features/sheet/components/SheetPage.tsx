c9fe77d502245747f0d77a852d7ab424da36378a
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-17 13:21:02 +0900

    シートの見せ方を変更。ホバーしたら情報を表示するようにした

--- src/features/sheet/components/SheetPage.tsx
+++ src/features/sheet/components/SheetPage.tsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { makeStyles } from '@mui/styles'
-import { Container, Tab, Box } from '@mui/material'
+import { Container, Grid, Tab, Box, Fade, Popper } from '@mui/material'
 import { TabContext, TabList } from '@mui/lab'
 import { H2 } from '@/components/Label/H2'
-import { ReadingRecord } from '../types'
-import { Card } from './Card'
+import { ReadingRecord, Record } from '../types'
 
 const useStyles = makeStyles({
-  month: {
-    fontWeight: 700,
-    background: 'rgba(37, 50, 56, 0.1)',
+  image: {
+    '&:hover': {
+      cursor: 'pointer',
+    },
   },
 })
 
@@ -22,16 +22,34 @@ interface Props {
 export const SheetPage: React.FC<Props> = ({ data, year }) => {
   const classes = useStyles()
   const router = useRouter()
-  const [value, setValue] = useState(year)
+  const [tab, setTab] = useState(year)
+  const [hoverBook, setHoverBook] = useState<Record>(null)
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue)
+    setTab(newValue)
     router.push(`/sheet/${newValue}`)
   }
+
+  const handleImageHover = (event) => {
+    setAnchorEl(event.currentTarget)
+    setOpen(true)
+    setHoverBook(JSON.parse(event.currentTarget.dataset.book))
+  }
+
+  const handleImageLeave = () => {
+    setOpen(false)
+    setHoverBook(null)
+  }
+
+  const [open, setOpen] = useState(false)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const canBeOpen = open && Boolean(anchorEl)
+  const id = canBeOpen ? 'transition-popper' : undefined
+
   return (
     <Container fixed>
       <H2 title="読書シート" />
-      <TabContext value={value}>
+      <TabContext value={tab}>
         <Box
           sx={{ marginBottom: '16px', borderBottom: 1, borderColor: 'divider' }}
         >
@@ -46,24 +64,49 @@ export const SheetPage: React.FC<Props> = ({ data, year }) => {
       </TabContext>
       <Box
         sx={{
-          maringTop: 5,
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'flex-start',
+          width: '100%',
         }}
       >
-        {data.map((book, i) => {
-          // 1月 1月 1月 ...のように同じ月を連続して表示させたくないための制御
-          let showMonth = i === 0 ? true : data[i - 1].month !== data[i].month
-          return (
-            <div key={i + book.title}>
-              <div className={classes.month}>
-                {showMonth ? book.month : '　'}
-              </div>
-              <Card record={book} />
-            </div>
-          )
-        })}
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 2, sm: 2, md: 3 }}>
+          {data.map((book, i) => {
+            return (
+              <Grid key={book.title + i} item xs={4} sm={3} md={2}>
+                <img
+                  className={classes.image}
+                  src={book.image}
+                  width={128}
+                  height={186}
+                  onMouseEnter={handleImageHover}
+                  onMouseLeave={handleImageLeave}
+                  data-book={JSON.stringify(book)}
+                />
+              </Grid>
+            )
+          })}
+
+          <Popper id={id} open={open} anchorEl={anchorEl} transition>
+            {({ TransitionProps }) => (
+              <Fade {...TransitionProps} timeout={350}>
+                <Box
+                  sx={{
+                    border: 1,
+                    p: 1,
+                    bgcolor: 'background.paper',
+                    width: '500px',
+                  }}
+                >
+                  {hoverBook && (
+                    <>
+                      <div>{hoverBook.title}</div>
+                      <div>{hoverBook.author}</div>
+                      <div>{hoverBook.memo}</div>
+                    </>
+                  )}
+                </Box>
+              </Fade>
+            )}
+          </Popper>
+        </Grid>
       </Box>
     </Container>
   )
