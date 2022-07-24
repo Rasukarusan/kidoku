f8a5229909abd443866e95193762a1ef7d81cc90
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-24 11:00:48 +0900

    hashをCOOKIEに保存する処理を追加

--- src/pages/api/hash.ts
+++ src/pages/api/hash.ts
@@ -1,11 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import nookies from 'nookies'
 import crypto from 'crypto'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { pass } = req.query
+  const pass = req.query.pass as string
   if (!pass) {
-    res.status(400).json('passを指定してください')
+    return res.status(400).json('passを指定してください')
   }
   const hashHex = crypto.createHash('sha256').update(pass, 'utf8').digest('hex')
+  nookies.set({ res }, 'myhash', hashHex, {
+    maxAge: 10 * 12 * 30 * 24 * 60 * 60, // 10年間
+    path: '/',
+  })
   res.status(200).json(hashHex)
 }
