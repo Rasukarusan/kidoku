854e07abbe6f9933c90e0d4a0a146455ba25cb2d
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-31 15:01:32 +0900

    ログインAPI修正

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -6,12 +6,15 @@ import { COOKIE_KEY_HASH } from '@/libs/constants'
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const pass = req.query.pass as string
   if (!pass) {
-    return res.status(400).json('passを指定してください')
+    return res.status(400).json(false)
   }
-  const hashHex = crypto.createHash('sha256').update(pass, 'utf8').digest('hex')
-  nookies.set({ res }, COOKIE_KEY_HASH, hashHex, {
-    maxAge: 10 * 12 * 30 * 24 * 60 * 60, // 10年間
-    path: '/',
-  })
-  return res.status(200).json(hashHex)
+  const hash = crypto.createHash('sha256').update(pass, 'utf8').digest('hex')
+  const isAuth = process.env.MY_HASH === hash
+  if (isAuth) {
+    nookies.set({ res }, COOKIE_KEY_HASH, hash, {
+      maxAge: 10 * 12 * 30 * 24 * 60 * 60, // 10年間
+      path: '/',
+    })
+  }
+  return res.status(200).json(isAuth)
 }
