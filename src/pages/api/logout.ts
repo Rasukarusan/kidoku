7b1d65856e78ee3b0c9dc97edbbb4a0a0d75bc00
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-31 14:06:12 +0900

    COOKIEの破棄方法を修正

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,7 +1,8 @@
-import nookies from 'nookies'
+import { destroyCookie } from 'nookies'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { COOKIE_KEY_HASH } from '@/libs/constants'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  nookies.destroy({ res }, 'myhash')
+  destroyCookie({ res }, COOKIE_KEY_HASH)
   res.status(200).json(true)
 }
