a60f75965e45d5c9718f2d325366eddf2685de76
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-31 13:59:25 +0900

    COOKIEのキー名を定数に変更

--- src/pages/api/hash.ts
+++ src/pages/api/hash.ts
@@ -1,6 +1,7 @@
 import nookies from 'nookies'
 import crypto from 'crypto'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { COOKIE_KEY_HASH } from '@/libs/constants'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const pass = req.query.pass as string
@@ -8,7 +9,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.status(400).json('passを指定してください')
   }
   const hashHex = crypto.createHash('sha256').update(pass, 'utf8').digest('hex')
-  nookies.set({ res }, 'myhash', hashHex, {
+  nookies.set({ res }, COOKIE_KEY_HASH, hashHex, {
     maxAge: 10 * 12 * 30 * 24 * 60 * 60, // 10年間
     path: '/',
   })
--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -2,9 +2,6 @@ import nookies from 'nookies'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  nookies.set({ res }, 'myhash', '', {
-    maxAge: -1,
-    path: '/',
-  })
+  nookies.destroy({ res }, 'myhash')
   res.status(200).json(true)
 }
