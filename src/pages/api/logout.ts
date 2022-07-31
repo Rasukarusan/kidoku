712c6262cba6145673a615f9952dc58990696b70
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-31 14:07:16 +0900

    Revert "COOKIEの破棄方法を修正"
    This reverts commit 7b1d65856e78ee3b0c9dc97edbbb4a0a0d75bc00.

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,8 +1,7 @@
-import { destroyCookie } from 'nookies'
+import nookies from 'nookies'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { COOKIE_KEY_HASH } from '@/libs/constants'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  destroyCookie({ res }, COOKIE_KEY_HASH)
+  nookies.destroy({ res }, 'myhash')
   res.status(200).json(true)
 }
