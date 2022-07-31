2056299f50b3aaedfbcd4e463db5a20f5dfd38e6
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-07-31 14:25:32 +0900

    COOKIEの削除処理を変更

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,7 +1,11 @@
 import nookies from 'nookies'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { COOKIE_KEY_HASH } from '@/libs/constants'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  nookies.destroy({ res }, 'myhash')
+  nookies.set({ res }, COOKIE_KEY_HASH, '', {
+    maxAge: 0,
+    path: '/',
+  })
   return res.status(200).json(true)
 }
