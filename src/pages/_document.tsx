0d5a5095e6e78251dec3f65ce7f1ce254f6a01c2
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-10-10 19:53:29 +0900

    add meta description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,10 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" />
+          <meta
+            name="description"
+            content="本のタイトルから著者・カテゴリ・書影を検索できます。"
+          />
           <link
             href="https://fonts.googleapis.com/earlyaccess/nicomoji.css"
             rel="stylesheet"
