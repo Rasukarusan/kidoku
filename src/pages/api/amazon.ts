6aac37406e4ccd7f31e09f3a3ad632deeb546db5
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-10-23 23:32:11 +0900

    amazon検索結果から画像DL、類似度の取得を実装

--- src/pages/api/amazon.ts
+++ src/pages/api/amazon.ts
@@ -1,24 +1,46 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import resemble from 'node-resemble-js'
 import * as cheerio from 'cheerio'
+import { downloadImage } from '@/libs/downloadImage'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const targetImage =
+    'http://books.google.com/books/content?id=AIazzgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+  ;(async () => {
+    return downloadImage(targetImage, `target.jpg`)
+  })()
   let html = ''
   const decoder = new TextDecoder()
   const body = await fetch(
-    'https://www.amazon.co.jp/s?k=software+design+2022%E5%B9%B47%E6%9C%88%E5%8F%B7'
+    'https://www.amazon.co.jp/s?k=%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88%E3%83%BB%E3%83%98%E3%82%A4%E3%83%AB%E3%83%BB%E3%83%A1%E3%82%A2%E3%83%AA%E3%83%BC+%E4%B8%8A'
   ).then((res) => res.body)
-
   const reader = body.getReader()
   function readChunk({ done, value }) {
     if (done) {
+      // amazon検索結果から画像URLを抽出
+      // 後にPromise.Allで画像一括DLするためPromise配列を返す
       const $ = cheerio.load(html)
-      $('.s-image').map((i, v) => {
-        console.log($(v).attr('alt'))
-        console.log($(v).attr('src'))
+      const reqs = $('.s-image').map((i, v) => {
+        const imageURL = $(v).attr('src')
+        const title = $(v).attr('alt')
+        return downloadImage(imageURL, `${title}.jpg`)
       })
+
+      // 画像を一括DLし、1つずつ類似度を取得
+      ;(async () => {
+        const results = await Promise.all(reqs)
+        results.forEach((image) => {
+          console.log(image)
+          resemble('./target.jpg')
+            .compareTo(image)
+            .onComplete((data) => {
+              console.log(data)
+            })
+        })
+      })()
       return
     }
     html += decoder.decode(value)
@@ -27,5 +49,6 @@ export default async function handler(
   }
   // 最初の値を読み込む
   reader.read().then(readChunk)
+
   return res.status(200).json({ body })
 }
