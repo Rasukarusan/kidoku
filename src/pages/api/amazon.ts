fb9b99b83440bed4dab32342ab0a32d5163eb7c0
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-10-30 17:38:30 +0900

    vercelでエラーが出たのでhtmlの取得をnode-fetchで行うように変更

--- src/pages/api/amazon.ts
+++ src/pages/api/amazon.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-constant-condition */
+import fetch from 'node-fetch'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {
   ASSET_PATH,
@@ -17,19 +18,6 @@ export const compareImage = async (image) => {
   return r
 }
 
-export const getHtml = async (url: string) => {
-  let html = ''
-  const body = await fetch(url).then((res) => res.body)
-  const decoder = new TextDecoder()
-  const reader = body.getReader()
-  while (true) {
-    const { done, value } = await reader.read()
-    if (done) break
-    html += decoder.decode(value)
-  }
-  return html
-}
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -39,9 +27,9 @@ export default async function handler(
     'https://app.rasukarusan.com/_next/image?url=http%3A%2F%2Fbooks.google.com%2Fbooks%2Fcontent%3Fid%3DHIGsDwAAQBAJ%26printsec%3Dfrontcover%26img%3D1%26zoom%3D1%26edge%3Dcurl%26source%3Dgbs_api&w=256&q=75',
     target
   )
-  const html = await getHtml(
+  const html = await fetch(
     'https://www.amazon.co.jp/s?k=%E3%83%9E%E3%83%B3%E3%82%AC%E3%81%A7%E3%82%8F%E3%81%8B%E3%82%8B+%E3%82%B7%E3%83%B3%E3%83%97%E3%83%AB%E3%81%A7%E6%AD%A3%E3%81%97%E3%81%84%E3%81%8A%E9%87%91%E3%81%AE%E5%A2%97%E3%82%84%E3%81%97%E6%96%B9'
-  )
+  ).then((res) => res.text())
   // 大体上位5件以内に見つかるので、処理速度向上のため絞る
   const { links, images } = await downloadAmazonImages(html, 5)
 
