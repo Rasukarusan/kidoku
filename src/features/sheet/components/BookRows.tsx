4c94cc42e683430defcd0e59dbb9b4b298d1867a
Rasukarusan <gta_youtube@yahoo.co.jp>
2022-12-24 12:25:42 +0900

    色修正

--- src/features/sheet/components/BookRows.tsx
+++ src/features/sheet/components/BookRows.tsx
@@ -21,8 +21,8 @@ export const BookRows: React.FC<Props> = ({ books }) => {
 
   return (
     <div className="overflow-x-auto relative mb-12">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <table className="w-full text-sm text-left text-gray-500 border">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
           <tr>
             <th scope="col" className="py-3 px-6">
               No
@@ -49,14 +49,14 @@ export const BookRows: React.FC<Props> = ({ books }) => {
         <tbody>
           {books.map((book, i) => (
             <tr
-              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-700 hover:cursor-pointer"
+              className="bg-white border-b hover:bg-gray-50 hover:cursor-pointer"
               key={`${book.title}-${i}`}
               onClick={() => onClickImage(book)}
             >
               <td className="py-4 px-6">{i + 1}</td>
               <th
                 scope="row"
-                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap"
               >
                 {truncate(book.title, 20)}
               </th>
