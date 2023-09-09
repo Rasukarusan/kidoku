3afeab2d0725978af5bd33cb556b1c3aaccb4943
Rasukarusan <7gta0you1tube000@gmail.com>
2023-09-09 18:44:42 +0900

    余分な記述を削除

--- src/features/global/util.ts
+++ src/features/global/util.ts
@@ -1,17 +1,15 @@
 export const login = async (pass: string): Promise<boolean> => {
-  return await fetch(`/api/login?pass=${pass}`, { method: 'POST' })
-    .then((res) => res.json())
-    .then((res) => res)
+  return await fetch(`/api/login?pass=${pass}`, { method: 'POST' }).then(
+    (res) => res.json()
+  )
 }
 
 export const logout = async (): Promise<boolean> => {
-  return await fetch(`/api/logout`, { method: 'POST' })
-    .then((res) => res.json())
-    .then((res) => res)
+  return await fetch(`/api/logout`, { method: 'POST' }).then((res) =>
+    res.json()
+  )
 }
 
 export const auth = async (): Promise<boolean> => {
-  return await fetch('/api/auth')
-    .then((res) => res.json())
-    .then((res) => res)
+  return await fetch('/api/auth').then((res) => res.json())
 }
