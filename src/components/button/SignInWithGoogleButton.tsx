e346c891c3cfa84cc20e8effa9098f1caa08b549
Rasukarusan <7gta0you1tube000@gmail.com>
2023-09-10 16:56:02 +0900

    ログインモーダルのUI修正

--- src/components/button/SignInWithGoogleButton.tsx
+++ src/components/button/SignInWithGoogleButton.tsx
@@ -1,10 +1,14 @@
 interface Props {
   onClick: () => void
+  className?: string
 }
-export const SignInWithGoogleButton: React.FC<Props> = ({ onClick }) => {
+export const SignInWithGoogleButton: React.FC<Props> = ({
+  onClick,
+  className = '',
+}) => {
   return (
     <button
-      className="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+      className={`px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150 ${className}`}
       onClick={onClick}
     >
       <img
