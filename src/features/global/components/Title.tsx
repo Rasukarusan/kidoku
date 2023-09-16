8f51d958176fce912c4ddb78b891f2b69fb3b08c
Rasukarusan <7gta0you1tube000@gmail.com>
2023-09-16 17:12:06 +0900

    タイトルからロゴを削除

--- src/features/global/components/Title.tsx
+++ src/features/global/components/Title.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link'
 import { Typography } from '@mui/material'
-import AutoStoriesIcon from '@mui/icons-material/AutoStories'
 
 export const Title: React.FC = () => {
   return (
     <>
-      <AutoStoriesIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
       <Link href="/" legacyBehavior>
         <Typography
           variant="h6"
