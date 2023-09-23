f2f9247250a5301c2c8513be599021a19a0ec055
Rasukarusan <7gta0you1tube000@gmail.com>
2023-09-23 19:36:31 +0900

    一部のページだけISRに変更

--- src/pages/[user]/sheets/[year].tsx
+++ src/pages/[user]/sheets/[year].tsx
@@ -1,12 +1,10 @@
 import prisma, { parse } from '@/libs/prisma'
 import dayjs from 'dayjs'
 import { SheetPage } from '@/features/sheet/components/SheetPage'
-import { authOptions } from '@/pages/api/auth/[...nextauth]'
-import { getServerSession } from 'next-auth'
 export default SheetPage
 
 type Props = { params: { year: string } }
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
   const { user: username, year } = context.params
   const user = await prisma.user.findUnique({
     where: { name: username },
@@ -16,8 +14,6 @@ export async function getServerSideProps(context) {
       notFound: true,
     }
   }
-  const session = await getServerSession(context.req, context.res, authOptions)
-  const isMine = session?.user?.id === user.id
   const userId = user.id
   const sheets = await prisma.sheets.findMany({
     where: { userId },
@@ -46,7 +42,7 @@ export async function getServerSideProps(context) {
       impression,
       is_public_memo,
     } = book
-    const memo = isMine || is_public_memo ? book.memo : ''
+    const memo = is_public_memo ? book.memo : ''
     return {
       id,
       userId,
@@ -66,5 +62,15 @@ export async function getServerSideProps(context) {
       year,
       sheets: sheets.map((sheet) => sheet.name),
     },
+    revalidate: 300,
+  }
+}
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { user: 'Rasukarusan', year: '2023' } },
+      { params: { user: 'Rasukarusan', year: '2022' } },
+    ],
+    fallback: 'blocking', // キャッシュが存在しない場合はSSR
   }
 }
