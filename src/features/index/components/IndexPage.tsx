b9c2bb7fd736b5ac2d5d7e9a3e5c1c3cfd6a3a10
Rasukarusan <7gta0you1tube000@gmail.com>
2023-09-28 05:28:38 +0900

    Lottieを導入

--- src/features/index/components/IndexPage.tsx
+++ src/features/index/components/IndexPage.tsx
@@ -1,5 +1,7 @@
 import { Container } from '@/components/layout/Container'
 import Link from 'next/link'
+import Lottie from 'lottie-react'
+import Inchworm from '../lottie_inchworm.json'
 
 interface Props {
   users: {
@@ -19,37 +21,42 @@ export const Row = ({ label, value }) => {
 export const IndexPage = ({ users }) => {
   return (
     <Container className="p-6">
-      {users.map((user, i) => (
-        <div key={user.name} className="flex items-center">
-          <Link href={`/${user.name}/sheets`} className="m-10 flex">
-            <div className="mr-4">
-              <img
-                src={user.image}
-                alt=""
-                className="rounded-full w-[100px] h-[100px] mb-2"
-              />
-              <div className="font-bold text-center">{user.name}</div>
+      <div className="flex items-start">
+        <div>
+          {users.map((user, i) => (
+            <div key={user.name} className="flex items-center">
+              <Link href={`/${user.name}/sheets`} className="m-10 flex">
+                <div className="mr-4">
+                  <img
+                    src={user.image}
+                    alt=""
+                    className="rounded-full w-[100px] h-[100px] mb-2"
+                  />
+                  <div className="font-bold text-center">{user.name}</div>
+                </div>
+              </Link>
+              <table className="border w-96">
+                <tbody>
+                  <Row label="Total" value={`${user.books.total}冊`} />
+                  <Row
+                    label="Category"
+                    value={
+                      user.books.categories.length === 0
+                        ? '-'
+                        : user.books.categories.map((category) => (
+                            <div key={`${category.name}-${category.percent}`}>
+                              {`${category.name}: ${category.percent}% `}
+                            </div>
+                          ))
+                    }
+                  />
+                </tbody>
+              </table>
             </div>
-          </Link>
-          <table className="border w-96">
-            <tbody>
-              <Row label="Total" value={`${user.books.total}冊`} />
-              <Row
-                label="Category"
-                value={
-                  user.books.categories.length === 0
-                    ? '-'
-                    : user.books.categories.map((category) => (
-                        <div key={`${category.name}-${category.percent}`}>
-                          {`${category.name}: ${category.percent}% `}
-                        </div>
-                      ))
-                }
-              />
-            </tbody>
-          </table>
+          ))}
         </div>
-      ))}
+        <Lottie animationData={Inchworm} />;
+      </div>
     </Container>
   )
 }
