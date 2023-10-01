ad4001c3d520d1a8b4e6df7d3ebe39fe2c8f8449
Rasukarusan <7gta0you1tube000@gmail.com>
2023-10-01 22:29:53 +0900

    refactor

--- src/pages/api/batch/vercel_blob.ts
+++ src/pages/api/batch/vercel_blob.ts
@@ -9,13 +9,14 @@ const get = async (request: NextApiRequest, response: NextApiResponse) => {
 }
 
 const post = async (request: NextApiRequest, response: NextApiResponse) => {
-  const books = await prisma.books.findMany({
-    where: { sheet_id: 1 },
-  })
+  const books = await prisma.books.findMany({ where: { sheet_id: 9 } })
   // const books = r.slice(0, 2)
   for (let i = 0; i < books.length; i++) {
     const book = books[i]
     const path = `${book.id}.webp`
+    if (!book.image.includes('http')) {
+      continue
+    }
     const res = await (await fetch(book.image)).arrayBuffer()
     // webpに変換し、VercelBlobにアップロード
     sharp(res)
@@ -25,9 +26,10 @@ const post = async (request: NextApiRequest, response: NextApiResponse) => {
         if (err) {
           throw err
         }
-        const { url } = await put('hoge.webp', info, {
+        const { url } = await put(path, info, {
           access: 'public',
         })
+        console.log(url)
         await prisma.books.update({
           where: { id: book.id },
           data: {
@@ -39,6 +41,11 @@ const post = async (request: NextApiRequest, response: NextApiResponse) => {
   return response.status(200).json({ result: true })
 }
 const del = async (request: NextApiRequest, response: NextApiResponse) => {
+  // 全削除
+  // const li = await list()
+  // li.blobs.forEach((v) => {
+  //   blob_del(v.url)
+  // })
   const url =
     'https://ne0h3gml3ggcpadq.public.blob.vercel-storage.com/hoge-PWQXtkUTOnlNgXwKu4rdTckqzqJgpB'
   await blob_del(url)
