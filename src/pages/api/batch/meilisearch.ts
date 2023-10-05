8f2f4a987e5ac9a65c0aaedd7f14a912012f3cff
Rasukarusan <7gta0you1tube000@gmail.com>
2023-10-05 22:45:16 +0900

    meilisearchに登録する情報を追加

--- src/pages/api/batch/meilisearch.ts
+++ src/pages/api/batch/meilisearch.ts
@@ -8,21 +8,43 @@ export default async function handler(
 ) {
   try {
     const books = await prisma.books.findMany({
-      select: { id: true, title: true },
+      select: {
+        id: true,
+        title: true,
+        author: true,
+        image: true,
+        is_public_memo: true,
+        memo: true,
+        user: { select: { name: true, image: true } },
+        sheet: { select: { name: true } },
+      },
     })
+    const b = books.map((book) => {
+      const { id, title, author, image, memo, is_public_memo, user, sheet } =
+        book
+      return {
+        id,
+        title,
+        author,
+        image,
+        memo: is_public_memo ? memo : '',
+        user: user.name,
+        sheet: sheet.name,
+      }
+    })
+
     const client = new MeiliSearch({
       host: process.env.MEILI_HOST,
       apiKey: process.env.MEILI_MASTER_KEY,
     })
-    const re = await client.index('books').addDocuments(books)
-    console.log(re)
+    const re = await client.index('books').addDocuments(b)
     // client
     //   .index('books')
     //   .search('金')
     //   .then((res) => console.log(res))
+    return response.status(200).json({ result: true })
   } catch (error) {
     console.error(error)
     return response.status(200).json({ result: false })
   }
-  return response.status(200).json({ result: true })
 }
