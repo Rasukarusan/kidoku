5332aa30c2011899c0781cbade887292dcbab0e6
Rasukarusan <7gta0you1tube000@gmail.com>
2023-10-18 00:08:04 +0900

    UI調整

--- src/components/input/SearchBox/AddModal.tsx
+++ src/components/input/SearchBox/AddModal.tsx
@@ -98,7 +98,7 @@ export const AddModal: React.FC<Props> = ({ open, item, books, onClose }) => {
   return (
     <Modal open={open} onClose={onClose} className="sm:w-1/2 h-3/4">
       <div className="flex flex-col justify-between h-full">
-        <div className="p-4">
+        <div className="p-4 bg-white">
           <div className="flex items-center">
             <ImagePicker
               img={book?.image}
