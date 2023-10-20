48afe6d2e689b3ba6048954a51539e602055f99b
Rasukarusan <7gta0you1tube000@gmail.com>
2023-10-20 21:38:26 +0900

    ヘッダーをmaterial uiから脱却

--- src/features/global/components/Header.tsx
+++ src/features/global/components/Header.tsx
@@ -1,8 +1,8 @@
 import useSWR from 'swr'
 import { fetcher } from '@/libs/swr'
 import { useRouter } from 'next/router'
-import { AppBar, Toolbar, Container } from '@mui/material'
 import { signOut, useSession } from 'next-auth/react'
+import { Container } from '@/components/layout/Container'
 import { LoginModal } from './LoginModal'
 import { motion, AnimatePresence, Variants } from 'framer-motion'
 import { useEffect, useRef, useState } from 'react'
@@ -56,86 +56,84 @@ export const Header = () => {
 
   return (
     <>
-      <AppBar position="fixed" color="primary" sx={{ boxShadow: 'none' }}>
-        <Container maxWidth="lg">
-          <Toolbar disableGutters>
-            <Link
-              href="/"
-              className="mr-8 font-bold tracking-[.3rem] no-underline font-['Nico_Moji'] text-xl"
-            >
-              <Logo className="w-8 h-8" />
-            </Link>
-            <SearchBox />
-            <div className="flex-grow"></div>
-            {session && (
-              <div className="relative" ref={dropdownRef}>
-                <button
-                  className="rounded-full bg-gray-200 w-[34px] h-[34px] sm:w-[40px] sm:h-[40px] sm:mr-4"
-                  onClick={() => setOpenMenu(!openMenu)}
-                >
-                  <img
-                    src={session.user.image}
-                    alt="ユーザーアイコン"
-                    className="rounded-full"
-                  />
-                </button>
-                <AnimatePresence>
-                  {openMenu && (
-                    <motion.ul
-                      initial="closed"
-                      animate="open"
-                      exit="closed"
-                      variants={variants}
-                      className="absolute z-[1000] right-4 m-0 min-w-max list-none overflow-hidden rounded-lg border bg-white bg-clip-padding text-left text-base shadow-lg"
-                    >
-                      <li className="border border-bottom-1 flex items-center px-4 py-2 hover:bg-neutral-100">
-                        <BookIcon className="w-[24px] h-[24px] text-slate-300 mr-2" />
-                        <Link
-                          className="block w-full whitespace-nowrap text-gray-600 bg-transparent text-sm"
-                          href={url}
-                          onClick={() => setOpenMenu(false)}
-                        >
-                          読書記録
-                        </Link>
-                      </li>
-                      <li className="border border-bottom-1 flex items-center px-4 py-2 hover:bg-neutral-100">
-                        <SettingsIcon className="w-[24px] h-[24px] text-slate-300 mr-2" />
-                        <Link
-                          className="block w-full whitespace-nowrap text-gray-600 bg-transparent text-sm"
-                          href="/settings/profile"
-                          onClick={() => setOpenMenu(false)}
-                        >
-                          アカウント設定
-                        </Link>
-                      </li>
-                      <li className="border border-bottom-1 flex items-center px-4 py-2 hover:bg-neutral-100">
-                        <ExitIcon className="w-[24px] h-[24px] text-slate-300 mr-2" />
-                        <button
-                          className="block w-full whitespace-nowrap text-gray-600 bg-transparent text-sm text-left"
-                          onClick={() => signOut()}
-                        >
-                          ログアウト
-                        </button>
-                      </li>
-                    </motion.ul>
-                  )}
-                </AnimatePresence>
-              </div>
-            )}
-            {session === null && (
-              <>
-                <button
-                  className="rounded-md bg-[#4f5b62] text-xs sm:text-sm px-4 py-2 sm:px-5 sm:py-2 font-bold"
-                  onClick={() => setOpen(true)}
-                >
-                  ログイン
-                </button>
-                <LoginModal open={open} onClose={() => setOpen(false)} />
-              </>
-            )}
-          </Toolbar>
+      <div className="bg-main text-white fixed top-0 w-full z-50">
+        <Container className="flex items-center p-2">
+          <Link
+            href="/"
+            className="mr-8 font-bold tracking-[.3rem] no-underline font-['Nico_Moji'] text-xl"
+          >
+            <Logo className="w-8 h-8" />
+          </Link>
+          <SearchBox />
+          <div className="flex-grow"></div>
+          {session && (
+            <div className="relative" ref={dropdownRef}>
+              <button
+                className="rounded-full bg-gray-200 w-[34px] h-[34px] sm:w-[40px] sm:h-[40px] sm:mr-4"
+                onClick={() => setOpenMenu(!openMenu)}
+              >
+                <img
+                  src={session.user.image}
+                  alt="ユーザーアイコン"
+                  className="rounded-full"
+                />
+              </button>
+              <AnimatePresence>
+                {openMenu && (
+                  <motion.ul
+                    initial="closed"
+                    animate="open"
+                    exit="closed"
+                    variants={variants}
+                    className="absolute z-[1000] right-4 m-0 min-w-max list-none overflow-hidden rounded-lg border bg-white bg-clip-padding text-left text-base shadow-lg"
+                  >
+                    <li className="border border-bottom-1 flex items-center px-4 py-2 hover:bg-neutral-100">
+                      <BookIcon className="w-[24px] h-[24px] text-slate-300 mr-2" />
+                      <Link
+                        className="block w-full whitespace-nowrap text-gray-600 bg-transparent text-sm"
+                        href={url}
+                        onClick={() => setOpenMenu(false)}
+                      >
+                        読書記録
+                      </Link>
+                    </li>
+                    <li className="border border-bottom-1 flex items-center px-4 py-2 hover:bg-neutral-100">
+                      <SettingsIcon className="w-[24px] h-[24px] text-slate-300 mr-2" />
+                      <Link
+                        className="block w-full whitespace-nowrap text-gray-600 bg-transparent text-sm"
+                        href="/settings/profile"
+                        onClick={() => setOpenMenu(false)}
+                      >
+                        アカウント設定
+                      </Link>
+                    </li>
+                    <li className="border border-bottom-1 flex items-center px-4 py-2 hover:bg-neutral-100">
+                      <ExitIcon className="w-[24px] h-[24px] text-slate-300 mr-2" />
+                      <button
+                        className="block w-full whitespace-nowrap text-gray-600 bg-transparent text-sm text-left"
+                        onClick={() => signOut()}
+                      >
+                        ログアウト
+                      </button>
+                    </li>
+                  </motion.ul>
+                )}
+              </AnimatePresence>
+            </div>
+          )}
+          {session === null && (
+            <>
+              <button
+                className="rounded-md bg-[#4f5b62] text-xs sm:text-sm px-4 py-2 sm:px-5 sm:py-2 font-bold"
+                onClick={() => setOpen(true)}
+              >
+                ログイン
+              </button>
+              <LoginModal open={open} onClose={() => setOpen(false)} />
+            </>
+          )}
         </Container>
-      </AppBar>
+      </div>
       <div style={{ marginBottom: '70px' }}></div>
     </>
   )
