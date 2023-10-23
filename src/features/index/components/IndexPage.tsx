ed60a4c16de34c43344f61d5d0fa1e747d7575b1
Rasukarusan <7gta0you1tube000@gmail.com>
2023-10-23 18:23:55 +0900

    Meilisearchをコンポーネント化

--- src/features/index/components/IndexPage.tsx
+++ src/features/index/components/IndexPage.tsx
@@ -2,14 +2,7 @@ import { Container } from '@/components/layout/Container'
 import Link from 'next/link'
 import Lottie from 'lottie-react'
 import Inchworm from '../lottie_inchworm.json'
-
-import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch'
-import { instantMeiliSearch } from '@meilisearch/instant-meilisearch'
-
-const searchClient = instantMeiliSearch(
-  process.env.NEXT_PUBLIC_HOST + '/api/meilisearch',
-  'hoge'
-)
+import { MeiliSearch } from '@/components/input/MeiliSearch'
 
 interface Props {
   users: {
@@ -29,34 +22,10 @@ export const Row = ({ label, value }) => {
   )
 }
 
-const Hit = ({ hit }) => {
-  console.log(hit)
-  return (
-    <div key={hit.id} className="m-4 flex items-center border p-4">
-      <Link
-        href={`/${hit.user}/sheets/${hit.sheet}`}
-        className="mr-4 min-w-[50px] text-center"
-      >
-        <img className="" src={hit.image} width="50" />
-      </Link>
-      <div>
-        <Link href={`/${hit.user}/sheets/${hit.sheet}`}>
-          <Highlight attribute="title" hit={hit} className="mb-1" />
-        </Link>
-        <div className="mb-1 text-xs">{hit.author}</div>
-        <Highlight attribute="memo" hit={hit} className="mb-1 text-xs" />
-      </div>
-    </div>
-  )
-}
-
 export const IndexPage = ({ users }) => {
   return (
     <Container className="p-6">
-      <InstantSearch indexName="books" searchClient={searchClient}>
-        <SearchBox className="border" />
-        <Hits hitComponent={Hit} />
-      </InstantSearch>
+      <MeiliSearch />
       <div className="sm:flex sm:items-start">
         <div>
           {users.map((user, i) => (
