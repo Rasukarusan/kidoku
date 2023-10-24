be4f39bcfcc2849ede45ef0f50bb4078190646e6
Rasukarusan <7gta0you1tube000@gmail.com>
2023-10-24 13:13:11 +0900

    シート追加ボタンを実装

--- src/features/sheet/components/Tabs.tsx
+++ src/features/sheet/components/Tabs.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/router'
+import { Modal } from '@/components/layout/Modal'
 
 interface Props {
   sheets: string[]
@@ -9,6 +10,13 @@ interface Props {
 export const Tabs: React.FC<Props> = ({ value, sheets, username }) => {
   const router = useRouter()
   const [tab, setTab] = useState(value)
+  const [open, setOpen] = useState(false)
+  const [newSheet, setNewSheet] = useState('')
+  const ref = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    ref.current?.focus()
+  }, [open])
 
   const onClick = (value: string) => {
     setTab(value)
@@ -28,6 +36,45 @@ export const Tabs: React.FC<Props> = ({ value, sheets, username }) => {
           {sheet}
         </button>
       ))}
+      <button
+        className="w-[90px] px-8 py-3 text-center text-sm uppercase text-gray-600 duration-300 ease-in hover:bg-gray-100"
+        onClick={() => {
+          setOpen(true)
+        }}
+      >
+        ＋
+      </button>
+      <Modal open={open} onClose={() => setOpen(false)} className="h-1/4 w-1/3">
+        <div className="p-10 text-center">
+          <div className="mb-4 text-2xl font-bold">新しいシートを作成</div>
+          <input
+            ref={ref}
+            className="mx-auto mb-4 block w-2/3 rounded-md border p-2"
+            type="text"
+            onChange={(e) => {
+              setNewSheet(e.target.value)
+            }}
+            value={newSheet}
+            placeholder="シート名を入力"
+          />
+          <button
+            className="rounded-md bg-blue-700 px-4 py-1 font-bold text-white disabled:bg-blue-300"
+            disabled={!newSheet}
+            onClick={async () => {
+              const res = await fetch('/api/sheets', {
+                method: 'POST',
+                body: JSON.stringify({ name: newSheet }),
+                headers: {
+                  Accept: 'application/json',
+                },
+              }).then((res) => res.json())
+              console.log(res)
+            }}
+          >
+            追加する
+          </button>
+        </div>
+      </Modal>
     </div>
   )
 }
--- src/pages/api/sheets.ts
+++ src/pages/api/sheets.ts
@@ -9,12 +9,23 @@ export default async (req, res) => {
       return res.status(200).json({ result: false, sheets: [] })
     }
     const userId = session.user.id
-    const sheets = await prisma.sheets.findMany({
-      where: { userId },
-      select: { id: true, name: true },
-      orderBy: [{ order: 'desc' }],
-    })
-    return res.status(200).json({ result: true, sheets })
+
+    if (req.method === 'GET') {
+      const sheets = await prisma.sheets.findMany({
+        where: { userId },
+        select: { id: true, name: true },
+        orderBy: [{ order: 'desc' }],
+      })
+      return res.status(200).json({ result: true, sheets })
+    } else if (req.method === 'POST') {
+      const { name } = JSON.parse(req.body)
+      // TODO: orderを調整
+      await prisma.sheets.create({
+        data: { userId, name },
+      })
+      return res.status(200).json({ result: true })
+    }
+    return res.status(200).json({ result: true })
   } catch (e) {
     console.error(e)
     res.status(400).json({ result: false })
