ef390063e6cc7e2c4ff48a77d58193a702c75656
Rasukarusan <7gta0you1tube000@gmail.com>
2023-10-26 00:09:28 +0900

    コメント一覧実装途中

--- src/features/index/components/IndexPage.tsx
+++ src/features/index/components/IndexPage.tsx
@@ -2,7 +2,8 @@ import { Container } from '@/components/layout/Container'
 import Link from 'next/link'
 import Lottie from 'lottie-react'
 import Inchworm from '../lottie_inchworm.json'
-import { MeiliSearch } from '@/components/input/MeiliSearch'
+import { truncate } from '@/utils/string'
+import dayjs from 'dayjs'
 
 interface Props {
   users: {
@@ -22,10 +23,75 @@ export const Row = ({ label, value }) => {
   )
 }
 
+interface CommentProps {
+  comment: {
+    title: string
+    text: string
+    updated: string
+    user: {
+      name: string
+      image: string
+    }
+  }
+}
+export const Comment: React.FC<CommentProps> = ({ comment }) => {
+  comment.title = '7つの習慣'
+  comment.text = `[期待]
+          ずっと読まないとなと思っていた。要約や動画の知識でこれからも語っていくのは限界が来ていた。
+          まこなり社長の3大読んだほうがいい本の最後の本。
+          [感想]
+          2023年ベスト1本になりそう。以下は5％ルールのために書いた第一章分。`
+  comment.updated =
+    dayjs().diff(dayjs().subtract(2, 'day'), 'day', false) + '日前'
+  const { text, user } = comment
+  return (
+    <div className="flex max-w-[400px] items-center p-4">
+      <div>
+        <img
+          className="mb-1 w-[82px] min-w-[82px] pr-4"
+          src="https://ne0h3gml3ggcpadq.public.blob.vercel-storage.com/23-LglOJLt5MmX7aOjxdhj3SmBPORucjC.webp"
+          alt=""
+        />
+        <div className="text-sm text-gray-700">{comment.title}</div>
+      </div>
+      <div>
+        <div className="mb-2 text-sm font-bold text-gray-600">
+          {truncate(comment.text, 120)}
+        </div>
+        <div className="flex items-center">
+          <img className="mr-2 h-8 w-8 rounded-full" src={user.image} alt="" />
+          <div>
+            <Link
+              className="text-sm text-gray-500 hover:underline"
+              href={`/${user.name}/sheets`}
+            >
+              {user.name}
+            </Link>
+            <div className="text-sm text-gray-500">{comment.updated}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export const IndexPage = ({ users }) => {
   return (
     <Container className="p-6">
-      <MeiliSearch />
+      <section>
+        <h2 className="p-2 text-2xl font-bold">Comments</h2>
+        <div className="flex">
+          {/* <Comment */}
+          {/* comment={{ */}
+          {/* user: { */}
+          {/* name: 'Rasukarusan', */}
+          {/* image: */}
+          {/* 'https://lh3.googleusercontent.com/a/ACg8ocJT-LCACb3glXijA-jQa27gYGIy3yRnsSo1TenXA833urs=s96-c', */}
+          {/* }, */}
+          {/* }} */}
+          {/* /> */}
+        </div>
+      </section>
       <div className="sm:flex sm:items-start">
         <div>
           {users.map((user, i) => (
