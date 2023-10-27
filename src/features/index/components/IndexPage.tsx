4ea616a308def708baa81424dd943fbde759d7fb
Rasukarusan <7gta0you1tube000@gmail.com>
2023-10-27 14:58:11 +0900

    トップ画面にコメント一覧を表示

--- src/features/index/components/IndexPage.tsx
+++ src/features/index/components/IndexPage.tsx
@@ -1,73 +1,38 @@
 import { Container } from '@/components/layout/Container'
 import Link from 'next/link'
-import Lottie from 'lottie-react'
-import Inchworm from '../lottie_inchworm.json'
 import { truncate } from '@/utils/string'
 import dayjs from 'dayjs'
 
 interface Props {
-  users: {
-    name: string
-    image: string
-    books: { total: number; categories: { name: string; percent: number }[] }
-    url: string
-  }[]
+  comments: CommentProps
 }
-
-export const Row = ({ label, value }) => {
-  return (
-    <tr className="border">
-      <td className="w-1/4 border bg-gray-100 p-2">{label}</td>
-      <td className="border p-2">{value}</td>
-    </tr>
-  )
-}
-
 interface CommentProps {
-  comment: {
-    title: string
-    text: string
-    updated: string
-    user: {
-      name: string
-      image: string
-    }
-  }
+  title: string
+  memo: string
+  image: string
+  updated: string
+  username: string
+  userImage: string
 }
-export const Comment: React.FC<CommentProps> = ({ comment }) => {
-  comment.title = '7つの習慣'
-  comment.text = `[期待]
-          ずっと読まないとなと思っていた。要約や動画の知識でこれからも語っていくのは限界が来ていた。
-          まこなり社長の3大読んだほうがいい本の最後の本。
-          [感想]
-          2023年ベスト1本になりそう。以下は5％ルールのために書いた第一章分。`
-  comment.updated =
-    dayjs().diff(dayjs().subtract(2, 'day'), 'day', false) + '日前'
-  const { text, user } = comment
+export const Comment: React.FC<{ comment: CommentProps }> = ({ comment }) => {
+  const { title, memo, image, username, userImage } = comment
+  const updated = dayjs().diff(dayjs(comment.updated), 'day', false) + '日前'
   return (
-    <div className="flex max-w-[400px] items-center p-4">
-      <div>
-        <img
-          className="mb-1 w-[82px] min-w-[82px] pr-4"
-          src="https://ne0h3gml3ggcpadq.public.blob.vercel-storage.com/23-LglOJLt5MmX7aOjxdhj3SmBPORucjC.webp"
-          alt=""
-        />
-        <div className="text-sm text-gray-700">{comment.title}</div>
+    <div className="flex items-center p-4">
+      <div className="pr-4  text-center">
+        <img className="mb-1 w-[82px] min-w-[82px]" src={image} alt="" />
       </div>
       <div>
-        <div className="mb-2 text-sm font-bold text-gray-600">
-          {truncate(comment.text, 120)}
+        <div className="mb-2 text-xs font-bold text-gray-600 sm:text-sm">
+          {truncate(memo, 120)}
         </div>
-        <div className="flex items-center">
-          <img className="mr-2 h-8 w-8 rounded-full" src={user.image} alt="" />
+        <div className="flex items-center text-xs text-gray-500 sm:text-sm ">
+          <img className="mr-2 h-8 w-8 rounded-full" src={userImage} alt="" />
           <div>
-            <Link
-              className="text-sm text-gray-500 hover:underline"
-              href={`/${user.name}/sheets`}
-            >
-              {user.name}
+            <Link className="hover:underline" href={`/${username}/sheets`}>
+              {username}
             </Link>
-            <div className="text-sm text-gray-500">{comment.updated}</div>
+            <div className="">{updated}</div>
           </div>
         </div>
       </div>
@@ -75,59 +40,17 @@ export const Comment: React.FC<CommentProps> = ({ comment }) => {
   )
 }
 
-export const IndexPage = ({ users }) => {
+export const IndexPage = ({ comments }) => {
   return (
     <Container className="p-6">
       <section>
         <h2 className="p-2 text-2xl font-bold">Comments</h2>
-        <div className="flex">
-          {/* <Comment */}
-          {/* comment={{ */}
-          {/* user: { */}
-          {/* name: 'Rasukarusan', */}
-          {/* image: */}
-          {/* 'https://lh3.googleusercontent.com/a/ACg8ocJT-LCACb3glXijA-jQa27gYGIy3yRnsSo1TenXA833urs=s96-c', */}
-          {/* }, */}
-          {/* }} */}
-          {/* /> */}
-        </div>
-      </section>
-      <div className="sm:flex sm:items-start">
-        <div>
-          {users.map((user, i) => (
-            <div key={user.name} className="sm:flex sm:items-center">
-              <Link href={user.url} className="m-4 flex justify-center sm:m-10">
-                <div className="mr-4 hover:underline hover:brightness-90">
-                  <img
-                    src={user.image}
-                    alt=""
-                    className="mb-2 h-[100px] w-[100px] rounded-full"
-                  />
-                  <div className="text-center font-bold">{user.name}</div>
-                </div>
-              </Link>
-              <table className="mb-4 w-96 border">
-                <tbody>
-                  <Row label="Total" value={`${user.books.total}冊`} />
-                  <Row
-                    label="Category"
-                    value={
-                      user.books.categories.length === 0
-                        ? '-'
-                        : user.books.categories.map((category) => (
-                            <div key={`${category.name}-${category.percent}`}>
-                              {`${category.name}: ${category.percent}% `}
-                            </div>
-                          ))
-                    }
-                  />
-                </tbody>
-              </table>
-            </div>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          {comments.map((comment) => (
+            <Comment key={comment.id} comment={comment} />
           ))}
         </div>
-        <Lottie animationData={Inchworm} />
-      </div>
+      </section>
     </Container>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,35 @@
 import { IndexPage } from '@/features/index/components/IndexPage'
 export default IndexPage
 
-import prisma from '@/libs/prisma'
+import prisma, { parse } from '@/libs/prisma'
 
 export const getStaticProps = async (ctx) => {
-  const users = await prisma.user.findMany({
+  const books = await prisma.books.findMany({
     include: {
-      books: { select: { category: true } },
-      sheets: {
-        select: { name: true, order: true },
-        orderBy: [{ order: 'desc' }],
+      user: { select: { name: true, image: true } },
+      sheet: {
+        select: { name: true },
       },
     },
-    take: 5,
+    where: { is_public_memo: true },
+    orderBy: [{ updated: 'desc' }],
+    take: 10,
+  })
+  const comments = []
+  parse(books).map((book) => {
+    comments.push({
+      id: book.id,
+      title: book.title,
+      memo: book.memo,
+      updated: book.updated,
+      image: book.image,
+      username: book.user.name,
+      userImage: book.user.image,
+    })
   })
   const result = []
-  users.map((user) => {
-    const categoryCount: Record<string, number> = {}
-    user.books.forEach((item) => {
-      if (!categoryCount[item.category]) {
-        categoryCount[item.category] = 0
-      }
-      categoryCount[item.category]++
-    })
-    const total = user.books.length
-    const categories = Object.entries(categoryCount)
-      .sort((a, b) => b[1] - a[1])
-      .slice(0, 3)
-      .map((v) => {
-        return {
-          name: v[0],
-          percent: Math.round((v[1] / total) * 100),
-        }
-      })
-    const sheet = user.sheets.length > 0 ? `${user.sheets[0].name}` : 'total'
-    result.push({
-      name: user.name,
-      image: user.image,
-      books: { total, categories },
-      url: `/${user.name}/sheets/${sheet}`,
-    })
-  })
-  result.sort((a, b) => b.books.total - a.books.total)
   return {
-    props: { users: result },
+    props: { comments },
     revalidate: 86400,
   }
 }
