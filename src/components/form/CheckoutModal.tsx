7a3e55aa3842d5116bb512ab90405d3a0162df03
Rasukarusan <7gta0you1tube000@gmail.com>
2023-11-26 20:26:52 +0900

    不要な記述を削除

--- src/components/form/CheckoutModal.tsx
+++ src/components/form/CheckoutModal.tsx
@@ -23,11 +23,9 @@ export const CheckoutModal: React.FC<Props> = ({
 }) => {
   const [clientSecret, setClientSecret] = useState('')
   useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
     fetch('/api/stripe/create-payment-intent', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ items: [{ id: 'xl-tshirt' }] }),
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret))
