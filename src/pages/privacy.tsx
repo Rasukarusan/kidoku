3b6bf0276a54b94f6ef265bd15b1e568ce202813
Rasukarusan <7gta0you1tube000@gmail.com>
2024-02-13 09:54:48 +0900

    ISR不要だったのでSSGに変更

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,10 +1,2 @@
 import { PrivacyPage } from '@/features/privacy/components/PrivacyPage'
-
 export default PrivacyPage
-
-export const getStaticProps = async (ctx) => {
-  return {
-    props: {},
-    revalidate: 300,
-  }
-}
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,10 +1,2 @@
 import { TermsPage } from '@/features/terms/components/TermsPage'
-
 export default TermsPage
-
-export const getStaticProps = async (ctx) => {
-  return {
-    props: {},
-    revalidate: 300,
-  }
-}
