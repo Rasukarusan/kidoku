38bd996690a81256d5650c4a0e99683078656958
Rasukarusan <7gta0you1tube000@gmail.com>
2024-02-26 10:47:27 +0900

    kvではなくtidbで保存する方式に変更

--- src/pages/api/barcode.ts
+++ src/pages/api/barcode.ts
@@ -1,7 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { kv } from '@vercel/kv'
 import { getServerSession } from 'next-auth'
 import { authOptions } from './auth/[...nextauth]'
+import prisma from '@/libs/prisma'
+import { EventType } from '@/types/event_queue'
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,17 +18,35 @@ export default async function handler(
       if (isMobile) {
         return res.status(200).json({ result: false })
       }
-      const id = session.user.id
-      const book = await kv.get(`${id}_barcode`)
-      await kv.del(`${id}_barcode`)
-      return res.status(200).json({ result: true, book })
+      const userId = session.user.id
+      const queue = await prisma.eventQueues.findFirst({
+        where: { userId, event: EventType.AddBook },
+      })
+      return res
+        .status(200)
+        .json({ result: true, book: JSON.parse(queue?.message) })
     }
     if (req.method === 'POST') {
       const body = JSON.parse(req.body)
       const { book } = body
-      const id = session.user.id
-      console.log(book, id)
-      await kv.set(`${id}_barcode`, book, { ex: 100 })
+      const message = JSON.stringify(book)
+      const userId = session.user.id
+      const result = await prisma.eventQueues.upsert({
+        where: {
+          userId_event: {
+            userId: userId,
+            event: EventType.AddBook,
+          },
+        },
+        update: {
+          message,
+        },
+        create: {
+          userId: userId,
+          event: EventType.AddBook,
+          message,
+        },
+      })
       return res.status(200).json(true)
     }
   } catch (e) {
---
+++ src/types/event_queue.ts
@@ -0,0 +1,3 @@
+export enum EventType {
+  AddBook = 'add_book',
+}
