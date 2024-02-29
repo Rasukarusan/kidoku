b571115d07b9d45f1faa92eb6704d46dfbcb0823
Rasukarusan <7gta0you1tube000@gmail.com>
2024-02-29 15:10:03 +0900

    user-auth実装

--- src/libs/pusher/client.ts
+++ src/libs/pusher/client.ts
@@ -1,6 +1,14 @@
 import Pusher from 'pusher-js'
 const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+  // userAuthentication: {
+  //   endpoint: '/api/pusher/user-auth',
+  //   transport: 'ajax',
+  //   params: { hoge: 'fofo' },
+  //   headers: { cussus: 'kasutamudayo' },
+  //   paramsProvider: null,
+  //   headersProvider: null,
+  // },
   channelAuthorization: {
     endpoint: '/api/pusher/auth',
     transport: 'ajax',
--- src/libs/pusher/util.ts
+++ src/libs/pusher/util.ts
@@ -7,6 +7,8 @@ export function getChannelName(userId: string) {
 
 /**
  * 認証
+ *
+ * getServerSessionで取得するuserIdとチャンネル名に含まれるuserIdが一致するかで判定
  */
 export function auth(userId: string, channelName: string) {
   return getChannelName(userId) === channelName
