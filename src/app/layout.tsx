5428c91ff679a72f894bca41475a3246aeff7270
Rasukarusan <7gta0you1tube000@gmail.com>
2024-03-02 23:14:54 +0900

    app router導入

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import '../pages/global.css'
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './src/app/**/*.tsx',
     './src/pages/**/*.tsx',
     './src/components/**/*.tsx',
     './src/features/**/*.tsx',
