7d70a580a89a45ef4cd7c4fd30fc8a0ea5d70747
Rasukarusan <7gta0you1tube000@gmail.com>
2024-04-26 11:38:07 +0900

    画像選択でURLで指定できるようにした

--- src/components/button/ImagePicker.tsx
+++ src/components/button/ImagePicker.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
+import { Modal } from '../layout/Modal'
+import { FaEdit, FaLink } from 'react-icons/fa'
 
 interface Props {
   img?: string
@@ -7,8 +9,32 @@ interface Props {
 export const ImagePicker: React.FC<Props> = ({ onImageLoad, img }) => {
   const ref = useRef(null)
   const [image, setImage] = useState(img)
+  const [base64, setBase64] = useState('')
+  const [open, setOpen] = useState(false)
 
-  // 画像選択
+  // URLから画像を読み込む
+  const loadImageAsBase64 = (url): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      const img = new Image()
+      img.onload = () => {
+        const canvas = document.createElement('canvas')
+        canvas.width = img.width
+        canvas.height = img.height
+        const ctx = canvas.getContext('2d')
+        ctx.drawImage(img, 0, 0)
+        const dataURL = canvas.toDataURL()
+        // Base64部分のみを返す
+        const base64Image = dataURL.split(',')[1]
+        setBase64(base64Image)
+        setImage(dataURL)
+      }
+      img.onerror = reject
+      img.crossOrigin = 'Anonymous' // CORSポリシー回避
+      img.src = url
+    })
+  }
+
+  // ファイルから画像選択
   const handleChange = (e) => {
     const file = e.target.files[0]
     if (!file) return
@@ -16,10 +42,15 @@ export const ImagePicker: React.FC<Props> = ({ onImageLoad, img }) => {
     setImage(url)
     const reader = new FileReader()
     reader.onload = (e) => {
-      const image = (e.target.result as string).split(',')[1]
-      onImageLoad && onImageLoad(image)
+      const base64Image = (e.target.result as string).split(',')[1]
+      setBase64(base64Image)
     }
     reader.readAsDataURL(file)
+  }
+
+  const onSubmit = async (e) => {
+    onImageLoad && onImageLoad(base64)
+    setOpen(false)
   }
 
   useEffect(() => {
@@ -29,9 +60,66 @@ export const ImagePicker: React.FC<Props> = ({ onImageLoad, img }) => {
   }, [img])
 
   return (
-    <button className="mr-4 w-1/3" onClick={() => ref.current.click()}>
-      <img className="mx-auto my-0 drop-shadow-lg" src={image} alt="" />
-      <input ref={ref} type="file" onChange={handleChange} className="hidden" />
-    </button>
+    <>
+      <button
+        className="relative mr-4 w-1/3 hover:brightness-75"
+        onClick={() => setOpen(true)}
+      >
+        <img className="mx-auto my-0 drop-shadow-lg" src={image} alt="" />
+        <input
+          ref={ref}
+          type="file"
+          onChange={handleChange}
+          className="hidden"
+        />
+        <div className="absolute top-1/2 mx-auto flex h-full w-full -translate-y-1/2 transform items-center justify-center opacity-0  hover:opacity-100  hover:brightness-75">
+          <FaEdit
+            size={25}
+            className="bg-transparent text-gray-100 opacity-100"
+          />
+        </div>
+      </button>
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+        className="w-full sm:w-[500px]"
+      >
+        <div className="mx-auto w-full rounded-md bg-white p-4 text-center sm:w-10/12">
+          <div className="mb-4 flex items-center justify-start rounded-md border-2 border-slate-300 px-2 py-2">
+            <FaLink className="mr-2 text-slate-500" />
+            <input
+              type="text"
+              onChange={async (e) => {
+                await loadImageAsBase64(e.target.value)
+              }}
+              placeholder="画像URLをペースト"
+              className="w-full placeholder:text-sm focus:outline-none"
+            />
+          </div>
+          <button
+            className="mb-4 hover:brightness-75"
+            onClick={() => ref.current.click()}
+          >
+            <img
+              className="mx-auto my-0 max-h-52 drop-shadow-lg"
+              src={image}
+              alt=""
+            />
+            <input
+              ref={ref}
+              type="file"
+              onChange={handleChange}
+              className="hidden"
+            />
+          </button>
+          <button
+            className="mx-auto block rounded-md bg-green-700 px-4 py-1 text-sm text-white"
+            onClick={onSubmit}
+          >
+            決定
+          </button>
+        </div>
+      </Modal>
+    </>
   )
 }
