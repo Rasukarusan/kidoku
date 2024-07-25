8c6692012ed4f5f1b6b33760633bc7e9e2d703c4
Rasukarusan <7gta0you1tube000@gmail.com>
2024-07-25 15:21:29 +0900

    ボタンUIを変更

--- src/features/sheet/components/BookDetailEditModal.tsx
+++ src/features/sheet/components/BookDetailEditModal.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Book } from '@/types/book'
 import { ToggleButton } from '@/components/button/ToggleButton'
 import { useSession } from 'next-auth/react'
@@ -63,134 +63,132 @@ export const BookDetailEditModal: React.FC<Props> = ({
     })
   }, [book])
   return (
-    <>
-      <div className="overflow-y-hidden p-4">
-        <div className="mb-4">
-          <div className="flex justify-center">
-            <ImagePicker
-              img={book.image}
-              onImageLoad={(image) => setBook({ ...book, image })}
-            />
-          </div>
-          <BookInputField
-            value={book?.title}
-            onChange={(e) => setBook({ ...book, title: e.target.value })}
-            label="タイトル"
-            tabIndex={1}
-            isChanged={diff.title}
-          />
-          <BookInputField
-            value={book?.author}
-            onChange={(e) => setBook({ ...book, author: e.target.value })}
-            label="著者"
-            tabIndex={2}
-            isChanged={diff.author}
+    <div className="overflow-y-hidden p-4">
+      <div className="mb-4">
+        <div className="flex justify-center">
+          <ImagePicker
+            img={book.image}
+            onImageLoad={(image) => setBook({ ...book, image })}
           />
         </div>
-        <div className="mb-4 flex items-center justify-between text-left">
-          <div className="max-w-[140px] sm:max-w-[200px]">
-            <BookCreatableSelectBox
-              label="カテゴリ"
-              defaultValue={{ value: book?.category, label: book?.category }}
-              options={options}
-              tabIndex={3}
-              isChanged={diff.category}
-              onChange={(newValue: { value: string; label: string }) => {
-                setBook({ ...book, category: newValue?.value ?? '-' })
-              }}
-            />
-          </div>
-          <BookSelectBox
-            value={book?.impression}
-            onChange={(e) => setBook({ ...book, impression: e.target.value })}
-            label="感想"
-            tabIndex={4}
-            isChanged={diff.impression}
+        <BookInputField
+          value={book?.title}
+          onChange={(e) => setBook({ ...book, title: e.target.value })}
+          label="タイトル"
+          tabIndex={1}
+          isChanged={diff.title}
+        />
+        <BookInputField
+          value={book?.author}
+          onChange={(e) => setBook({ ...book, author: e.target.value })}
+          label="著者"
+          tabIndex={2}
+          isChanged={diff.author}
+        />
+      </div>
+      <div className="mb-4 flex items-center justify-between text-left">
+        <div className="max-w-[140px] sm:max-w-[200px]">
+          <BookCreatableSelectBox
+            label="カテゴリ"
+            defaultValue={{ value: book?.category, label: book?.category }}
+            options={options}
+            tabIndex={3}
+            isChanged={diff.category}
+            onChange={(newValue: { value: string; label: string }) => {
+              setBook({ ...book, category: newValue?.value ?? '-' })
+            }}
           />
-          <BookDatePicker
-            value={dayjs(book.finished).format('YYYY-MM-DD')}
-            onChange={(e) =>
+        </div>
+        <BookSelectBox
+          value={book?.impression}
+          onChange={(e) => setBook({ ...book, impression: e.target.value })}
+          label="感想"
+          tabIndex={4}
+          isChanged={diff.impression}
+        />
+        <BookDatePicker
+          value={dayjs(book.finished).format('YYYY-MM-DD')}
+          onChange={(e) =>
+            setBook({
+              ...book,
+              finished: dayjs(e.target.value).format('YYYY-MM-DD'),
+            })
+          }
+          label="読了日"
+          tabIndex={5}
+          isChanged={diff.finished}
+        />
+      </div>
+      <div className="mb-4">
+        <div className="mb-2 flex items-center">
+          <Label text="メモ" className="mb-0 mr-2" />
+          <div data-tooltip-id="toggle-memo-tooltip">
+            <HintIcon />
+          </div>
+          <Tooltip id="toggle-memo-tooltip" className="!w-[300px]">
+            &quot;**&quot;で囲んだ文字はマスキングされ、他のユーザーには&quot;*****&quot;と表示されるようになります。例：「**マスク**さん」は「*****さん」と表示されます。
+          </Tooltip>
+        </div>
+        <BookInputField
+          rows={8}
+          value={book?.memo}
+          onChange={(e) => setBook({ ...book, memo: e.target.value })}
+          tabIndex={6}
+          isChanged={diff.memo}
+        />
+      </div>
+      <div className="mb-4 items-start justify-between sm:flex">
+        <div>
+          <ToggleButton
+            label="メモを公開する"
+            checked={book.is_public_memo}
+            onChange={() => {
               setBook({
                 ...book,
-                finished: dayjs(e.target.value).format('YYYY-MM-DD'),
+                is_public_memo: !book.is_public_memo,
               })
-            }
-            label="読了日"
-            tabIndex={5}
-            isChanged={diff.finished}
+            }}
+            disabled={false}
+            className="mb-2"
           />
-        </div>
-        <div className="mb-4">
-          <div className="mb-2 flex items-center">
-            <Label text="メモ" className="mb-0 mr-2" />
-            <div data-tooltip-id="toggle-memo-tooltip">
-              <HintIcon />
-            </div>
-            <Tooltip id="toggle-memo-tooltip" className="!w-[300px]">
-              &quot;**&quot;で囲んだ文字はマスキングされ、他のユーザーには&quot;*****&quot;と表示されるようになります。例：「**マスク**さん」は「*****さん」と表示されます。
-            </Tooltip>
-          </div>
-          <BookInputField
-            rows={8}
-            value={book?.memo}
-            onChange={(e) => setBook({ ...book, memo: e.target.value })}
-            tabIndex={6}
-            isChanged={diff.memo}
-          />
-        </div>
-        <div className="items-start justify-between sm:flex">
-          <div>
-            <ToggleButton
-              label="メモを公開する"
-              checked={book.is_public_memo}
-              onChange={() => {
-                setBook({
-                  ...book,
-                  is_public_memo: !book.is_public_memo,
-                })
-              }}
-              disabled={false}
-              className="mb-2"
-            />
-            {process.env.NEXT_PUBLIC_FLAG_KIDOKU_1 === 'true' && (
-              <div className="mb-2 flex items-center">
-                <ToggleButton
-                  label="課金によるメモの解放を許可する"
-                  checked={book.is_purchasable}
-                  onChange={() => {
-                    setBook({
-                      ...book,
-                      is_purchasable: !book.is_purchasable,
-                    })
-                  }}
-                  disabled={book.is_public_memo}
-                  className="mr-1"
-                />
-                <div data-tooltip-id="toggle-purchase-tooltip">
-                  <HintIcon />
-                </div>
-                <Tooltip
-                  id="toggle-purchase-tooltip"
-                  className="!sm:w-[400px] !w-[350px] text-xs"
-                >
-                  「ON」に設定することで、他のユーザーが課金することにより、
-                  非公開のメモを閲覧できます。公開されるのは課金したユーザーのみで、また、他の非公開メモは解放されません。
-                </Tooltip>
+          {process.env.NEXT_PUBLIC_FLAG_KIDOKU_1 === 'true' && (
+            <div className="mb-2 flex items-center">
+              <ToggleButton
+                label="課金によるメモの解放を許可する"
+                checked={book.is_purchasable}
+                onChange={() => {
+                  setBook({
+                    ...book,
+                    is_purchasable: !book.is_purchasable,
+                  })
+                }}
+                disabled={book.is_public_memo}
+                className="mr-1"
+              />
+              <div data-tooltip-id="toggle-purchase-tooltip">
+                <HintIcon />
               </div>
-            )}
-          </div>
-          <div className="flex justify-end">
-            <button
-              className="pr-2 text-right text-xs font-bold text-red-600"
-              onClick={onDelete}
-            >
-              削除する
-            </button>
-          </div>
+              <Tooltip
+                id="toggle-purchase-tooltip"
+                className="!sm:w-[400px] !w-[350px] text-xs"
+              >
+                「ON」に設定することで、他のユーザーが課金することにより、
+                非公開のメモを閲覧できます。公開されるのは課金したユーザーのみで、また、他の非公開メモは解放されません。
+              </Tooltip>
+            </div>
+          )}
+        </div>
+        <div className="flex justify-end">
+          <button
+            className="pr-2 text-right text-xs font-bold text-red-600"
+            onClick={onDelete}
+          >
+            削除する
+          </button>
         </div>
       </div>
       <button
-        className="flex h-12 w-full items-center justify-center rounded-md bg-green-600 px-4 py-1 font-bold text-white hover:bg-green-700 disabled:bg-green-800"
+        className="mx-auto flex h-10 w-full items-center justify-center rounded-md bg-green-600 px-4 py-1 font-bold text-white hover:bg-green-700 disabled:bg-green-800"
         onClick={onClick}
         tabIndex={6}
         disabled={loading}
@@ -200,6 +198,6 @@ export const BookDetailEditModal: React.FC<Props> = ({
         )}
         <span>保存する</span>
       </button>
-    </>
+    </div>
   )
 }
