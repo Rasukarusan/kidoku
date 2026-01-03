091e89855b68d14ed215bb5f3f79b8d5662fbcbb
Rasukarusan <7gta0you1tube000@gmail.com>
2026-01-03 17:32:50 +0900

    利用していない処理を削除

--- apps/api/src/modules/software-design/software-design.module.ts
+++ apps/api/src/modules/software-design/software-design.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SoftwareDesignResolver } from './software-design.resolver';
 import { SoftwareDesignService } from './software-design.service';
-import { SoftwareDesignController } from './software-design.controller';
-import { SoftwareDesignRepository } from './software-design.repository';
-import { AuthModule } from '../../infrastructure/auth/auth.module';
-import { DatabaseModule } from '../../infrastructure/database/database.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule],
-  providers: [
-    SoftwareDesignResolver,
-    SoftwareDesignService,
-    SoftwareDesignRepository,
-  ],
-  controllers: [SoftwareDesignController],
+  providers: [SoftwareDesignResolver, SoftwareDesignService],
   exports: [SoftwareDesignService],
 })
 export class SoftwareDesignModule {}
